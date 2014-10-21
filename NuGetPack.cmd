2d4291349ee4ba87bc9c3686fc61889cfbe0b533
Maxime Lemaitre <maxime.lemaitre@outlook.com>
2014-10-21 08:16:59 +0200

    Move nuspec file

--- NuGetPack.cmd
+++ NuGetPack.cmd
@@ -1,2 +1,3 @@
 SET NUGET=.nuget\nuget
-%NUGET% pack src\Toppler\Toppler.csproj -OutputDirectory dist -Build -Prop Configuration=Release
+%NUGET% pack src\Toppler_Net40\Toppler_Net40.csproj  -OutputDirectory dist -Build -Prop Configuration=Release
+%NUGET% pack src\Toppler\Toppler.csproj  -OutputDirectory dist -Build -Prop Configuration=Release
